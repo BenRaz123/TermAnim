8572747f36a127f9c22cb55fd230f6dc7d7db87f
BenRaz <81690487+BenRaz123@users.noreply.github.com>
2022-08-10 15:55:32 -0400

    Finished Refactoring

--- Animation.py
+++ Animation.py
@@ -1,70 +1,49 @@
+# Imports
+
 import os
+import sys
 import colorama
-from colorama import Fore
 from colorama import Style
+from colorama import Fore
 from time import sleep
 
+# Misc
+
 colorama.init(autoreset=True)
-a = []
-b= ""
-astr = ""
-top3 = 0
-myBool =True
-print("\nStarting In ...")
-sleep(1)
-print(f"{Fore.RED}\n3")
-sleep(1)
-print(f"{Fore.YELLOW}2")
-sleep(1)
-print(f"{Fore.GREEN}1!\n")
-sleep(1.3)
-arr = [Fore.RED, Fore.MAGENTA, Fore.BLUE, Fore.GREEN, Fore.YELLOW]
+
+# Variables
+
+leftSide = ""
+rightSide = ""
+termWidth = os.get_terminal_size().columns
+sleepTime = 1/termWidth
+possibleColors = [Fore.MAGENTA, Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.YELLOW, Fore.RED]
+
 for i in range(3):
-   size = os.get_terminal_size()
-   cols = size.columns
-   color = 0
-   for i in range(cols):
-      a += "\\" # Backslash
-   for i in range(cols):
-      top3 += 1
-      if top3 == 3:
-         color += 1
-         top3 = 0
-      if color == 5:
-         color = 0
-      astr = ""
-      a.pop(len(a)-1)
-      size = os.get_terminal_size()
-      cols = size.columns
-      b += "#"
-      for i in a:
-         astr += i
-      print(f"{Style.DIM}{astr}", end="")
-      print(f"{arr[color]}{b}")
-      sleep(0.015)
-   a = []
-   b= ""
-   astr = ""
-   myBool =True
-   for i in range(cols):
-      a += "#"
-   for i in range(cols):
-      top3 += 1
-      if top3 == 3:
-         color += 1
-         top3 = 0
-      if color == 5:
-         color = 0
-      astr = ""
-      size = os.get_terminal_size()
-      cols = size.columns
-      a.pop(len(a)-1)
-      b += "\\" # Backlash
-      for i in a:
-         astr += i
-      print(f"{Style.DIM}{b}", end="")
-      print(f"{arr[color]}{astr}")
-      sleep(0.015)
-   a = []
-   b = ""
-   astr = ""
+    colorKey = 0
+    for j in range(termWidth):
+        if i % 3 == 0:
+            colorKey += 1
+        if colorKey > len(possibleColors)-1:
+            colorKey = 0
+        rightSide += "#"
+        leftSide = ""
+        leftSideSize = termWidth - len(rightSide)
+        for i in range(leftSideSize):
+            leftSide += "\\"
+        print(f"{Style.DIM}{leftSide}{Style.NORMAL}{possibleColors[colorKey]}{rightSide}")
+        sleep(sleepTime)
+    colorKey = 0
+    for j in range(termWidth):
+        if i % 3 == 0:
+            colorKey += 1
+        if colorKey > len(possibleColors)-1:
+            colorKey = 0
+        leftSide += "\\"
+        rightSide = ""
+        rightSideSize = termWidth - len(leftSide)
+        for i in range(rightSideSize):
+            rightSide += "#"
+        print(f"{Style.DIM}{leftSide}{Style.NORMAL}{possibleColors[colorKey]}{rightSide}")
+        sleep(sleepTime)
+
